96ab952a12f8ac785e5a2fa002e90d5b4bce1bcf
RileyBrodie <riley.brodie@my.jcu.edu.au>
2019-09-05 12:11:10 +1000

    Rework score grading system into a function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,18 @@
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    score_result = score_grader(score)
+    print(score_result)
+
+
+def score_grader(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
