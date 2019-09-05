c9b1479b2ed5a5210bd18df11f487427fd8bec00
RileyBrodie <riley.brodie@my.jcu.edu.au>
2019-09-05 11:38:24 +1000

    Refactor program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,10 +1,23 @@
-"""Riley Brodie"""
-min_length = 2
-max_length = 10
+def main():
+    """Riley Brodie"""
+    min_length = 2
+    max_length = 10
 
-password = str(input("Enter a password"))
-while min_length > len(password) or len(password) > max_length:
-    print("please enter a password between 2 and 10 characters")
+    password = get_password(max_length, min_length)
+
+    asterisks_conversion(password)
+
+
+def asterisks_conversion(password):
+    print("*" * len(password))
+
+
+def get_password(max_length, min_length):
     password = str(input("Enter a password"))
+    while min_length > len(password) or len(password) > max_length:
+        print("please enter a password between 2 and 10 characters")
+        password = str(input("Enter a password"))
+    return password
 
-print("*" * len(password))
+
+main()
