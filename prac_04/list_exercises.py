cf83bf0a0d967861b200cc81bb9f1f17f5fe21e9
Riley <riley.brodie@my.jcu.edu.au>
2019-09-17 18:24:41 +1000

    Print access granted/denied from user input

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,8 @@ print("The {} number is {}".format("first", numbers[0]),
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
              'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
              'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter your username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
