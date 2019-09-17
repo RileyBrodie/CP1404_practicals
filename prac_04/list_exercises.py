74aba86e9df5335ada58f31e55c7c54adee97772
Riley <riley.brodie@my.jcu.edu.au>
2019-09-17 18:20:31 +1000

    Add usernames list and reform interesting facts strings

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,8 +3,13 @@ for i in range(1, 6):
     number = int(input("Number: "))
     numbers.append(number)
 print(numbers)
-print("The {} number is {}".format("first", numbers[0]), "\n"
-      "The {} number is {}".format("last", numbers[-1]), "\n"
-      "The {} number is {}".format("smallest", min(numbers)), "\n"
-      "The {} number is {}".format("largest", max(numbers)), "\n"
-      "The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+print("The {} number is {}".format("first", numbers[0]),
+      "\nThe {} number is {}".format("last", numbers[-1]),
+      "\nThe {} number is {}".format("smallest", min(numbers)),
+      "\nThe {} number is {}".format("largest", max(numbers)),
+      "\nThe average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
+"""Security Checker"""
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
