e1df43ca2381d1724cc009b680461d7f3ceed74c
Riley <riley.brodie@my.jcu.edu.au>
2019-09-17 17:56:57 +1000

    Add interesting facts

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,3 +3,8 @@ for i in range(1, 6):
     number = int(input("Number: "))
     numbers.append(number)
 print(numbers)
+print("The {} number is {}".format("first", numbers[0]), "\n"
+      "The {} number is {}".format("last", numbers[-1]), "\n"
+      "The {} number is {}".format("smallest", min(numbers)), "\n"
+      "The {} number is {}".format("largest", max(numbers)), "\n"
+      "The average of the numbers is {}".format(sum(numbers)/len(numbers)))
