282932ea479cd7d61dc4600cba3753fd58a1a20a
Riley <riley.brodie@my.jcu.edu.au>
2019-09-19 15:29:11 +1000

    Classes added

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -10,9 +10,9 @@ for num in range(1, quick_pick_lines + 1):
         random_number = (randint(1, 45))
         while random_number in QPS:
             random_number = (randint(1, 45))
-        QPS.append(random_number)
+        QPS.append(int(random_number))
     QPS.sort()
-    ALL_QUICK_PICKS.append(QPS)
+    ALL_QUICK_PICKS.append(str(QPS))
     print("{:2} {:2} {:2} {:2} {:2} {:2}".format
           (QPS[0], QPS[1], QPS[2], QPS[3], QPS[4], QPS[5]))
     QPS = []
