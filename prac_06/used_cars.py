c45b61137f3cdd47a876fad913cce3f258af87c6
Riley <riley.brodie@my.jcu.edu.au>
2019-09-24 22:12:20 +1000

    Add interchangeable names

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -3,19 +3,16 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.name = my_car
+    my_car = Car("my_car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
 
-    limo = Car(100)
-    limo.name = limo
+    limo = Car("limo", 100)
     limo.add_fuel(20)
     print("fuel =", limo.fuel)
     limo.drive(115)
     print("odo =", limo.odometer)
-
 
 
 main()
