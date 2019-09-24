07efe804eb2e3b4d987934e9f78d25c8a84f8725
Riley Brodie <riley.brodie@my.jcu.edu.au>
2019-09-24 12:17:14 +1000

    Add max_length for output formatting

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,4 +8,11 @@ for word in text:
     except KeyError:
         words[word] = 1
 
-print(words)
+max_length = 0
+for word in words:
+    word_length = len(word)
+    if word_length > max_length:
+        max_length = word_length
+
+for word in words:
+    print("{:{}} = {}".format(word, max_length, words[word]))
