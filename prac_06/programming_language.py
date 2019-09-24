925a850b96875666361ebd40fe96fdaf3c2b2c6b
Riley <riley.brodie@my.jcu.edu.au>
2019-09-25 00:46:38 +1000

    Complete task

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,17 @@
 class ProgrammingLanguage:
 
-    def __init__(self, typing, reflecting, year):
+    def __init__(self, name, typing, reflecting, year):
+        self.name = name
         self.typing = typing
         self.reflecting = reflecting
         self.year = year
 
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}" \
+            .format(self.name, self.typing, self.reflecting, self.year)
+
     def is_dynamic(self):
-        if "Dynamic" in self:
+        if "Dynamic" in self.typing:
             return True
         else:
             return False
