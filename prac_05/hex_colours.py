365f3e3410a952970e94775281e7ae0f1712534d
Riley <riley.brodie@my.jcu.edu.au>
2019-09-24 21:49:49 +1000

    Complete task

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-HEX_COLOURS = {"AliceBlue": "#f0f8ff", "BlueViolet": "#8a2be2", "Chocolate": "#d2691e", "DarkOrange": "#ff8c00",
-               "Firebrick": "#b22222", "LawnGreen": "#7cfc00", "Moccasin": "#ffe4b5"}
-# print(HEX_COLOURS)
-for colour in HEX_COLOURS:
-    print("{:8} is {}".format(colour, HEX_COLOURS[colour]))
-
-colour = input("Enter colour: ")
-while colour != "":
-    if colour in HEX_COLOURS:
-        print(colour, "is", HEX_COLOURS[colour])
-=======
 COLOUR_HEXES = {"Cyan1": "#00ffff", "DarkKhaki": "#bdb76b", "DarkOliveGreen": "#556b2f", "DarkOrange": "#ff8c00",
                 "DarkOrchid": "#9932cc", "LawnGreen": "#7cfc00", "LightBlue": "#add8e6"}
-# print(STATE_NAMES)
+# print(COLOUR_HEXES)
 for colour in COLOUR_HEXES:
     print("{:3} is {}".format(colour, COLOUR_HEXES[colour]))
 
@@ -20,7 +8,6 @@ colour = input("Enter colour: ")
 while colour != "":
     if colour in COLOUR_HEXES:
         print(colour, "is", COLOUR_HEXES[colour])
->>>>>>> origin/master
     else:
         print("Invalid colour")
     colour = input("Enter colour: ")
